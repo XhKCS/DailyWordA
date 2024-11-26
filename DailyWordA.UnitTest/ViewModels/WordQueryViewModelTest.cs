7a78d33178dcb33cafa06402d371a0fcf3664262
Yrugit <577054257@qq.com>
2024-11-26 09:42:56 +0800

    更新了文本翻译与单词查找的单元测试

---
+++ DailyWordA.UnitTest/ViewModels/WordQueryViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using DailyWordA.Library.Models;
+using DailyWordA.Library.ViewModels;
+
+namespace DailyWordA.UnitTest.ViewModels;
+
+public class WordQueryViewModelTest {
+    [Fact]
+    public async Task SetParameter_Default() {
+        var wordQueryViewModel = new WordQueryViewModel(null);
+        Assert.Equal(FilterType.EnglishWordFilter,
+            wordQueryViewModel.Filter.Type);
+        Assert.True(string.IsNullOrWhiteSpace(wordQueryViewModel
+            .Filter.QueryText));
+
+        var wordQuery = new WordQuery {
+            Word = "navigate", CnMeaning = "v. 导航"
+        };
+        wordQueryViewModel.SetParameter(wordQuery);
+        
+        Assert.Equal(FilterType.EnglishWordFilter,
+            wordQueryViewModel.Filter.Type);
+        Assert.Equal(wordQuery.Word,
+            wordQueryViewModel.Filter.QueryText);
+    }
+}
